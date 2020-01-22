dfd7d81daa20e0fe833c41ee8f2ea2cac512e03c
python39TBD <48202262+python39TBD@users.noreply.github.com>
2020-01-22 22:34:46 +0800

    生产数据库

--- coolcat/wsgi.py
+++ coolcat/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coolcat.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coolcat.prod')
 
 application = get_wsgi_application()
